b4cfa923cdad5d82b3cecaad7d1053cfe699c77a
Théo Segonds <theo.segonds@inria.fr>
2017-11-26 00:44:09 +0100

    Add poppy install script

---
+++ stage2/03-install-poppy/01-run.sh
@@ -0,0 +1,3 @@
+curl -L https://raw.githubusercontent.com/poppy-project/raspoppy/master/raspoppyfication.sh -o /tmp/raspoppyfication.sh
+chmod +x /tmp/raspoppyfication.sh
+sudo /tmp/raspoppyfication.sh
