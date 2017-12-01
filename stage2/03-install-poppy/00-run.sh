92fe5563949c85890a9497ea6e0d0daa7297c402
Théo Segonds <theo.segonds@inria.fr>
2017-12-01 10:42:17 +0100

    Add on_chroot

--- stage2/03-install-poppy/00-run.sh
+++ stage2/03-install-poppy/00-run.sh
@@ -1,3 +1,8 @@
+
+on_chroot << EOF
+set +e
+set -x
 curl -L https://raw.githubusercontent.com/poppy-project/raspoppy/master/raspoppyfication.sh -o /tmp/raspoppyfication.sh
 chmod +x /tmp/raspoppyfication.sh
 sudo /tmp/raspoppyfication.sh
+EOF
